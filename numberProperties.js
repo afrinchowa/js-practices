7aed76bc828df5aacf7393f6ba208eb4476ffe2c
chowa <arfinchowa524@gmail.com>
2025-05-22 23:58:17 +0600

    number properties

---
+++ numberProperties.js
@@ -0,0 +1,16 @@
+// JavaScript MAX_VALUE
+const max = Number.MAX_VALUE;
+console.log(max);
+// JavaScript MIN_VALUE
+const min = Number.MIN_VALUE;
+console.log(min);
+// JavaScript NaN
+const NaN = Number.NaN;
+console.log(NaN);
+// JavaScript NEGATIVE_INFINITY
+const negativeInfinity = Number.NEGATIVE_INFINITY;
+console.log(negativeInfinity);
+// JavaScript POSITIVE_INFINITY
+const positiveInfinity = Number.POSITIVE_INFINITY;
+console.log(positiveInfinity);
+
